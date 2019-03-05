2344bd6408ebf7b80ca5306a4e57f03442c65b6d
Artem Pyankov <artpyankov@gmail.com>
2019-03-05 22:19:50 +0500

    Improve setup

--- task-1.rb
+++ task-1.rb
@@ -45,8 +45,8 @@ def collect_stats_from_users(report, users_objects, &block)
   end
 end
 
-def work
-  file_lines = File.read('data.txt').split("\n")
+def work(file_path)
+  file_lines = File.read(file_path).split("\n")
 
   users = []
   sessions = []
@@ -145,6 +145,12 @@ def work
   File.write('result.json', "#{report.to_json}\n")
 end
 
+module MemoryMeasure
+  def self.call
+    `ps -o rss= -p #{Process.pid}`.to_i
+  end
+end
+
 class TestMe < Minitest::Test
   def setup
     File.write('result.json', '')
@@ -171,33 +177,29 @@ session,2,3,Chrome 20,84,2016-11-25
   end
 
   def test_result
-    profiling_result = RubyProf.profile { work }
+    work('data.txt')
 
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-    assert_equal expected_result, File.read('result.json')
+    assert_equal expected_result, File.read('result.json')    
+  end
+end
 
-    postfix = RubyProf.measure_mode == RubyProf::MEMORY ? 'memory' : 'time'
-    printer = RubyProf::CallTreePrinter.new(profiling_result)
-    printer.print(path: '.', profile: "profile_#{postfix}")
+if ENV['MEASURE']
+  puts '=' * 20
+  before_mem = after_mem = profiling_result = nil
+  time = Benchmark.realtime do
+    before_mem = MemoryMeasure.call
+    profiling_result = RubyProf.profile { work(ENV['MEASURE']) }
+    after_mem = MemoryMeasure.call
   end
 
-  def test_regression
-    before_mem = after_mem = nil
-    time = Benchmark.realtime do
-      before_mem = used_memory
-      1000.times { work }
-      after_mem = used_memory
-    end
+  puts "Time taken: #{time}"
 
-    assert time < 0.8, "Expect execution time to be less than 0.8, got #{time}"
-    mem_diff = after_mem - before_mem
-    puts "RSS diff #{mem_diff} KB"
-    assert mem_diff < 1700, "Expect to not use more than 1700 KB of memory, got #{mem_diff} KB"
-  end
+  mem_diff = after_mem - before_mem
+  puts "RSS diff #{mem_diff} KB"
+  puts '=' * 20
 
-  private
-
-  def used_memory
-    `ps -o rss= -p #{Process.pid}`.to_i
-  end
+  postfix = RubyProf.measure_mode == RubyProf::MEMORY ? 'memory' : 'time'
+  printer = RubyProf::CallTreePrinter.new(profiling_result)
+  printer.print(path: '.', profile: "profile_#{postfix}")
 end
