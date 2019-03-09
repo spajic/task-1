57473ab6dd8956ac6f796965727ef545948a04b1
Sergey Pustovalov <pustserg@yandex.ru>
2019-03-10 00:14:35 +0300

    Final optimization

--- task-1.rb
+++ task-1.rb
@@ -5,166 +5,32 @@ require 'pry'
 require 'date'
 require 'minitest/autorun'
 
+require 'ruby-prof'
 require 'benchmark'
 
-class User
-  attr_reader :attributes, :sessions
+require './user'
+require './work'
 
-  def initialize(attributes:, sessions:)
-    @attributes = attributes
-    @sessions = sessions
+if ARGV[0] == 'run'
+  profiling_result = nil
+
+  before_mem = `ps -o rss= -p #{Process.pid}`.to_i
+  total_time = Benchmark.realtime do
+    profiling_result = RubyProf.profile { Work.work('data_large.txt', 'large_result.json') }
   end
+  after_mem = `ps -o rss= -p #{Process.pid}`.to_i
+
+  mem_diff = (after_mem - before_mem) / 1024
+
+  puts "Total time #{total_time.round(4)}"
+  puts "Mem diff #{mem_diff}"
+  printer = RubyProf::CallTreePrinter.new(profiling_result)
+  printer.print(path: '.', profile: "profile_memory")
 end
-
-def parse_user(fields)
-  # fields = user.split(',')
-  parsed_result = {
-    'id' => fields[1],
-    'first_name' => fields[2],
-    'last_name' => fields[3],
-    'age' => fields[4],
-  }
-end
-
-def parse_session(fields)
-  # fields = session.split(',')
-  parsed_result = {
-    'user_id' => fields[1],
-    'session_id' => fields[2],
-    'browser' => fields[3],
-    'time' => fields[4],
-    'date' => fields[5],
-  }
-end
-
-def collect_stats_from_users(report, users_objects, &block)
-  stats_time = Benchmark.realtime do
-    users_objects.each do |user|
-      user_key = "#{user.attributes['first_name']}" + ' ' + "#{user.attributes['last_name']}"
-      report['usersStats'][user_key] ||= {}
-      report['usersStats'][user_key] = report['usersStats'][user_key].merge(block.call(user))
-    end
-  end
-  puts "Stats time #{stats_time.round(4)}"
-end
-
-def work(filename = 'data_large.txt')
-  file_lines = File.read(filename).split("\n")
-  puts "Handle file_lines #{file_lines.size} count"
-
-  users = []
-  sessions = []
-
-  lines_handle_time = Benchmark.realtime do
-    # file_lines[0..2000000].each do |line|
-    file_lines.each do |line|
-      cols = line.split(',')
-      users.push(parse_user(cols)) if cols[0] == 'user'
-      sessions.push(parse_session(cols)) if cols[0] == 'session'
-    end
-  end
-
-  puts "Lines handled in #{lines_handle_time.round(4)}"
-
-  # Отчёт в json
-  #   - Сколько всего юзеров +
-  #   - Сколько всего уникальных браузеров +
-  #   - Сколько всего сессий +
-  #   - Перечислить уникальные браузеры в алфавитном порядке через запятую и капсом +
-  #
-  #   - По каждому пользователю
-  #     - сколько всего сессий +
-  #     - сколько всего времени +
-  #     - самая длинная сессия +
-  #     - браузеры через запятую +
-  #     - Хоть раз использовал IE? +
-  #     - Всегда использовал только Хром? +
-  #     - даты сессий в порядке убывания через запятую +
-
-  report = {}
-
-  report[:totalUsers] = users.count
-
-  # Подсчёт количества уникальных браузеров
-  uniq_browsers_count_time = Benchmark.realtime do
-    report['uniqueBrowsersCount'] = sessions.uniq { |session| session['browser'] }.count
-  end
-
-  puts "uniq_browsers_count_time eq #{uniq_browsers_count_time.round(4)}"
-
-  report['totalSessions'] = sessions.count
-
-  all_browsers_time = Benchmark.realtime do
-    report['allBrowsers'] =
-      sessions
-        .map { |s| s['browser'].upcase }
-        .uniq
-        .sort
-        .join(',')
-  end
-  puts "all_browsers_time #{all_browsers_time.round(4)}"
-
-  # Статистика по пользователям
-  users_objects = []
-  grouped_sessions = sessions.group_by { |session| session['user_id'] }
-
-  build_users_time = Benchmark.realtime do
-    users.each do |user|
-      attributes = user
-      user_sessions = grouped_sessions[user['id']] || []
-      user_object = User.new(attributes: attributes, sessions: user_sessions)
-      users_objects.push(user_object)
-    end
-  end
-  puts "Users builded in #{build_users_time.round(4)}"
-
-  report['usersStats'] = {}
-
-  # Собираем количество сессий по пользователям
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'sessionsCount' => user.sessions.count }
-  end
-
-  # Собираем количество времени по пользователям
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'totalTime' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.sum.to_s + ' min.' }
-  end
-
-  # Выбираем самую длинную сессию пользователя
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'longestSession' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.max.to_s + ' min.' }
-  end
-
-  # Браузеры пользователя через запятую
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'browsers' => user.sessions.map {|s| s['browser']}.map {|b| b.upcase}.sort.join(', ') }
-  end
-
-  # Хоть раз использовал IE?
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'usedIE' => user.sessions.map{|s| s['browser']}.any? { |b| b.upcase =~ /INTERNET EXPLORER/ } }
-  end
-
-  # Всегда использовал только Chrome?
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ } }
-  end
-
-  # Даты сессий через запятую в обратном порядке в формате iso8601
-  collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{ |s| s['date'] }.sort.reverse }
-  end
-
-  File.write('result.json', "#{report.to_json}\n")
-end
-
-puts "start work #{Time.now}"
-work
-puts "end work #{Time.now}"
 
 class TestMe < Minitest::Test
   def setup
-    File.write('result.json', '')
+    File.write('test_result.json', '')
     File.write('data.txt',
 'user,0,Leida,Cira,0
 session,0,0,Safari 29,87,2016-10-23
@@ -188,8 +54,8 @@ session,2,3,Chrome 20,84,2016-11-25
   end
 
   def test_result
-    work('data.txt')
+    Work.work('data.txt', 'test_result.json')
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
-    assert_equal expected_result, File.read('result.json')
+    assert_equal expected_result, File.read('test_result.json')
   end
 end
