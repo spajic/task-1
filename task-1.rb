78703a8a3ae02957c58290f26f5e0bd76b8772d8
Aleksandr Shelestov <shoroh362@gmail.com>
2019-03-09 03:19:22 +0300

    Remove Date.parse Optimize Array#split Symbolize strings and so on.

--- task-1.rb
+++ task-1.rb
@@ -1,71 +1,106 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'date'
 require 'minitest/autorun'
 require 'minitest/benchmark'
 require 'ruby-prof'
 
 RubyProf.measure_mode = RubyProf::MEMORY
 
+USER_KEYS = {
+  1 => :id,
+  2 => :first_name,
+  3 => :last_name,
+  4 => :age
+}
+
+SESSION_KEYS = {
+  1 => :user_id,
+  2 => :session_id,
+  3 => :browser,
+  4 => :time,
+  5 => :date
+}
+
 class User
+  class Sessions
+    attr_reader :sessions
+
+    def initialize(sessions)
+      @sessions = sessions
+    end
+
+    def sessions_time
+      @sessions_time ||= sessions.map { |s| s[:time] }.map!(&:to_i)
+    end
+
+    def sessions_dates
+      sessions.map do |s|
+        s[:date]
+      end.sort_by! do |date_string|
+        date_string.rstrip!.delete('-')
+      end.reverse!
+    end
+  end
+
   attr_reader :attributes, :sessions
 
   def initialize(attributes:, sessions:)
     @attributes = attributes
-    @sessions = sessions
+    @sessions = Sessions.new(sessions)
   end
 
   def key
-    "#{attributes['first_name']} #{attributes['last_name']}"
+    "#{attributes[:first_name]} #{attributes[:last_name]}"
   end
 
   def stats
-    {
-      'sessionsCount' => sessions.count,
-      'totalTime' => "#{sessions_time.sum.to_s} min.",
-      'longestSession' => "#{sessions_time.max.to_s} min.",
-      'browsers' => sessions_browsers.join(', '),
-      'usedIE' => sessions_browsers.any? { |b| b =~ /INTERNET EXPLORER/ },
-      'alwaysUsedChrome' => sessions_browsers.all? { |b| b =~ /CHROME/ },
-      'dates' => sessions_dates
-    }
+    @stats ||= begin
+      browser_string = sessions_browsers.join(', ')
+
+      {
+        :'sessionsCount' => sessions.sessions.count,
+        :'totalTime' => "#{sessions.sessions_time.sum} min.",
+        :'longestSession' => "#{sessions.sessions_time.max} min.",
+        :'browsers' => browser_string,
+        :'usedIE' => browser_string.include?('INTERNET EXPLORER'),
+        :'alwaysUsedChrome' => sessions_browsers.all? { |b| b =~ /CHROME/ },
+        :'dates' => sessions.sessions_dates
+      }
+    end
   end
 
   def sessions_browsers
-    @sessions_browsers ||= sessions.map { |s| s['browser'].upcase! }.sort!
-  end
-
-  private
-
-  def sessions_time
-    @sessions_time ||= sessions.map { |s| s['time'] }.map!(&:to_i)
-  end
-
-  def sessions_dates
-    @sessions_dates ||= sessions.map { |s| Date.parse(s['date']) }.sort!.reverse!.map(&:iso8601)
+    @sessions_browsers ||= sessions.sessions.map { |s| s[:browser].upcase! }.sort!
   end
 end
 
 def parse_user(user)
-  fields = user.split(',')
-  parsed_result = {
-    'id' => fields[1],
-    'first_name' => fields[2],
-    'last_name' => fields[3],
-    'age' => fields[4],
-  }
+  order = 0
+  fields = {}
+  user.split(',') do |value|
+    if order == 0
+      order += 1
+      next
+    end
+    fields[USER_KEYS[order]] = value
+    order += 1
+  end
+  fields
 end
 
 def parse_session(session)
-  fields = session.split(',')
-  parsed_result = {
-    'user_id' => fields[1],
-    'session_id' => fields[2],
-    'browser' => fields[3],
-    'time' => fields[4],
-    'date' => fields[5],
-  }
+  order = 0
+  fields = {}
+  session.split(',') do |value|
+    if order == 0
+      order += 1
+      next
+    end
+    fields[SESSION_KEYS[order]] = value
+    order += 1
+  end
+  fields
 end
 
 def work(lines, file = nil)
@@ -77,16 +112,21 @@ def work(lines, file = nil)
   report['usersStats'] = {}
   unique_browsers = []
 
-  IO.foreach(file || 'data_large.txt').slice_before do |line|
+  file = IO.foreach(file || 'data_large.txt')
+  if lines
+    file = file.take(lines)
+  end
+
+  file.slice_before do |line|
     line.start_with?('user')
   end.each do |user_and_sessions|
     user_line, *sessions_lines = user_and_sessions
-    sessions = sessions_lines.map! { |session_line| parse_session(session_line) }
+    sessions = sessions_lines.map!(&method(:parse_session))
     user = User.new(attributes: parse_user(user_line.rstrip!), sessions: sessions)
 
     report['usersStats'][user.key] = user.stats
     report['totalUsers'] += 1
-    report['totalSessions'] += user.sessions.count
+    report['totalSessions'] += user.stats[:'sessionsCount']
 
     user.sessions_browsers.each do |browser|
       unique_browsers << browser unless unique_browsers.include?(browser)
@@ -114,38 +154,26 @@ def work(lines, file = nil)
   File.write('result.json', "#{report.to_json}\n")
 end
 
-# class BenchmarkWork < Minitest::Benchmark
-#   def self.bench_range
-#     [1, 10, 100, 1_000, 10_000, 100_000]
-#   end
-#
-#   def bench_work
-#     assert_performance_constant 0.99 do |x|
-#       work(x)
-#     end
-#   end
-# end
+class BenchmarkWork < Minitest::Benchmark
+  def self.bench_range
+    [1, 10, 100, 1_000, 10_000, 100_000]
+  end
+
+  def bench_work
+    assert_performance_linear(0.99, &method(:work))
+  end
+end
 
 
 def test_memory
-  result = RubyProf.profile { work(20_000) }
+  result = RubyProf.profile { work(20_000, 'data_large.txt') }
   printer = RubyProf::CallTreePrinter.new(result)
   printer.print(path: '.', profile: 'profile')
   # printer = RubyProf::GraphPrinter.new(result)
   # printer.print(STDOUT, :min_percent => 2)
 end
 
-test_memory
-
-class MemoryLeakTest < Minitest::Test
-  def test_memory
-    result = RubyProf.profile { work(20_000) }
-    # printer = RubyProf::CallTreePrinter.new(result)
-    # printer.print(path: '.', profile: 'profile')
-    printer = RubyProf::GraphPrinter.new(result)
-    printer.print(STDOUT, :min_percent => 2)
-  end
-end
+# test_memory
 
 class TestMe < Minitest::Test
   def setup
@@ -173,7 +201,7 @@ session,2,3,Chrome 20,84,2016-11-25
   end
 
   def test_result
-    work(18, 'data.txt')
+    work(false,'data.txt')
 
     expected_result = '{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}' + "\n"
     assert_equal expected_result, File.read('result.json')
