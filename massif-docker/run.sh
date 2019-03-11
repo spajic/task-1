359609cbf6cf9f4e325690c8e34d120ea085104e
Alex Emelyanov <holyketzer@gmail.com>
2019-03-11 20:03:44 +0300

    Measure memory with Valgrind

--- massif-docker/run.sh
+++ massif-docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd):/tmp holyketzer/ruby-valgrind valgrind --tool=massif ruby run.rb
+docker run -it -v $(pwd):/tmp -e DATA=data/data_20k.txt holyketzer/ruby-valgrind valgrind --tool=massif ruby run.rb
