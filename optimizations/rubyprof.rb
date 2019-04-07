f6c65cea626271ee81989bdb8528d914f4aa5b42
donasktello <shvetsov1988@gmail.com>
2019-04-07 13:57:11 +0300

    RubyProf wall measure Step1;

---
+++ optimizations/rubyprof.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/parser'
+require_relative '../spec/stdout_to_file'
+require 'ruby-prof'
+
+GC.disable
+
+save_stdout_to_file('rubyprof_wall.txt') do
+  RubyProf.measure_mode = RubyProf::WALL_TIME
+
+  result = RubyProf.profile do
+    work('data_65kb.txt')
+  end
+
+  printer = RubyProf::FlatPrinter.new(result)
+  printer.print($stdout)
+end
