e8c26eacffcd031c264a76cbc57aea37d85885b0
donasktello <shvetsov1988@gmail.com>
2019-04-10 22:29:54 +0300

    step3 removed redundant logic for parsing Date;

--- lib/parser.rb
+++ lib/parser.rb
@@ -146,7 +146,7 @@ def work(filename)
 
   # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, users_objects) do |user|
-    { 'dates' => user.sessions.map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user.sessions.map{|s| s['date']}.sort.reverse }
   end
 
   File.write("#{$support_dir}/result.json", "#{report.to_json}\n")
