ae2cc7ced6386dfcfadefe034d23d9d693d504b3
donasktello <shvetsov1988@gmail.com>
2019-04-13 11:54:58 +0300

    refactoring;

--- lib/parser.rb
+++ lib/parser.rb
@@ -33,45 +33,35 @@ def work(filename)
   dates = []
   browsers = []
 
-File.open("#{$support_dir}/result.json", 'a') do |f|
-  CSV.foreach("#{$support_dir}/#{filename}", row_sep: "\n") do |row|
-    if row[0] == 'user'
-      # report[:totalUsers] += 1
+  File.open("#{$support_dir}/result.json", 'a') do |f|
+    CSV.foreach("#{$support_dir}/#{filename}", row_sep: "\n") do |row|
+      if row[0] == 'user'
+        if sessions_count > 0
+          user = "\"#{row[2]} #{row[3]}\":{" << SESSIONS << sessions_count.to_s << "\"," <<
+              TOTAL_TIME << total_time.to_s << " min.\"," << LONGEST_SESSION << longest_session.to_s << " min.\"," <<
+              BROWSERS << browsers.sort!.join(', ').upcase! << "\"," << USED_IE << "#{used_ie}" << "\"," << ALWAYS_CHROME << "#{always_chrome}" <<
+              "\"," << DATES << dates.to_s << '},'
 
-      if sessions_count > 0
-        user = "\"#{row[2]} #{row[3]}\":{" << SESSIONS << sessions_count.to_s << "\"," <<
-            TOTAL_TIME << total_time.to_s << " min.\"," << LONGEST_SESSION << longest_session.to_s << " min.\"," <<
-            BROWSERS << browsers.sort!.join(', ').upcase! << "\"," << USED_IE << "#{used_ie}" << "\"," << ALWAYS_CHROME << "#{always_chrome}" <<
-            "\"," << DATES << dates.to_s << '},'
+          f.write(user)
 
-        f.write(user)
+          longest_session = total_time = sessions_count = 0
+          uniqueBrowsers.clear
+          dates.clear
+          browsers.clear
+          always_chrome = used_ie = false
+        end
+      else
+        uniqueBrowsers << row[3]
 
-        longest_session = total_time = sessions_count = 0
-        uniqueBrowsers.clear
-        dates.clear
-        browsers.clear
-        always_chrome = used_ie = false
+        sessions_count += 1
+        total_time += row[4].to_i
+        longest_session = row[4].to_i if longest_session < row[4].to_i
+        browsers << row[3]
+        used_ie = true if used_ie || row[3].start_with?('INTERNET')
+        always_chrome = false if used_ie || !always_chrome || !row[3].start_with?('CHROME')
+        dates << row[5]
       end
-    else
-      # report['allBrowsers'] << row[3] << EMPTY_COMMA
-      # report['totalSessions'] += 1
-      uniqueBrowsers << row[3]
-
-      sessions_count += 1
-      total_time += row[4].to_i
-      longest_session = row[4].to_i if longest_session < row[4].to_i
-      browsers << row[3]
-      used_ie = true if used_ie || row[3].start_with?('INTERNET')
-      always_chrome = false if used_ie || !always_chrome || !row[3].start_with?('CHROME')
-      dates << row[5]
     end
   end
-
-  # report['usersStats'].each_key { |name| report['usersStats'][name]['totalTime'] = report['usersStats'][name]['totalTime'].to_s + ' min.' }
-  # report['allBrowsers'].upcase!
-  # report['uniqueBrowsersCount'] = uniqueBrowsers.count
-  #
-  # File.write("#{$support_dir}/result.json", report.to_json << "\n")
-end
 
 end
