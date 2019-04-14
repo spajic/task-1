acd8d9db48b073c22cd41d44737fb0f5d3c721b7
Vlad Filiucov <filiucov@mail.ru>
2019-04-14 10:55:51 +0100

    ADD some performance tests

--- spec/performance_spec.rb
+++ spec/performance_spec.rb
@@ -1,37 +1,37 @@
 require_relative '../task-1.rb'
 
 RSpec.describe "Performance" do
-  describe '#parse_user' do
-    xit 'works' do
-      user1 = "user,0,Hazel,Margarete,19"
-      user2 = "user,0,Hazel,Margarete,19"
-
+  # This file is using helpers provided by rspec-benchmark gem https://github.com/piotrmurach/rspec-benchmark
+  describe 'Processing a file with 100 lines' do
+    it 'allocates 1935 objects' do
       expect {
-        parse_user(user1)
-        parse_user(user2)
-      }.to perform_allocation(12)
+        work('sample_data/100_lines.txt')
+      }.to perform_allocation(1935)
     end
 
-    it 'works' do
-      user1 = "user,0,Hazel,Margarete,19"
-      user2 = "user,0,Hazel,Margarete,19"
-      DEL = "user,".freeze
-
+    it 'performs under 1 ms' do
       expect {
-        user1.delete!(DEL)
-        user2.delete!(DEL)
-      }.to perform_allocation(3)
+        work('sample_data/100_lines.txt')
+      }.to perform_under(6).ms
     end
 
-    it 'curs' do
-      PAT = 'string,'.freeze
+    it 'performs at least 1150 iterations per second' do
       expect {
-        'string,two'.delete_prefix!(PAT)
-        'string,two'.delete_prefix!(PAT)
-        'string,two'.delete_prefix!(PAT)
-        'string,two'.delete_prefix!(PAT)
-        'string,two'.delete_prefix!(PAT)
-      }.to perform_allocation(5)
+        work('sample_data/100_lines.txt')
+      }.to perform_at_least(1150).within(0.4).warmup(0.2).ips
+    end
+
+    it 'performs constants' do
+      expect {
+        work('sample_data/100_lines.txt')
+      }.to perform_linear
+    end
+
+    # Not sure how to do this to get consistent result
+    xit 'performs linear' do
+      expect { |n, i|
+        work("sample_data/#{n}_lines.txt")
+      }.to perform_linear.in_range(100, 300).ratio(100)
     end
   end
 end
